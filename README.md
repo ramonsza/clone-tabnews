e6ebcc49c307cc622050bdebf14d6e81db78fd32
Ramon Souza <rsza@DESKTOP-E8EIT34>
2024-10-09 20:55:01 -0300

    Adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de aperfeiçoamento em programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-  return <h1>meme de 🐦</h1>
+function Home() {
+  return <h1>meme de 🐦</h1>;
 }
 
-export default Home
+export default Home;
