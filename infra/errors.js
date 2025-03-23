200f12959f938dedb2dd0f80bc5c135978f8fa3c
ramonsza <rsza@DESKTOP-E8EIT34>
2025-03-23 14:21:34 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -75,3 +75,25 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Não foi possível encontrar este recurso no sistema", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action =
+      action ||
+      "Verifique se os parâmetros enviados na consulta estão corretos";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
