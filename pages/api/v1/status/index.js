917bd9339df1935bef63a28951e3ad119e47e65c
ramonsza <rsza@DESKTOP-E8EIT34.localdomain>
2026-03-23 20:01:07 -0300

    refactor: explicitly return in `/api/v1/status` on `GET` handler

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -47,5 +47,5 @@ async function getHandler(request, response) {
     statusObject,
   );
 
-  response.status(200).json(secureOutputValues);
+  return response.status(200).json(secureOutputValues);
 }
