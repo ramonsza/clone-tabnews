504fa0fedfdbf4c2ae02c381a95782ea86f433a8
ramonsza <rsza@DESKTOP-E8EIT34.localdomain>
2026-03-24 09:25:44 -0300

    test: assert `email` is updated in database after `PATCH /api/v1/users/[username]`

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -268,6 +268,11 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+
+      const userInDatabase = await user.findOneByUsername(
+        createdUniqueUser.username,
+      );
+      expect(userInDatabase.email).toBe("uniqueEmail2@email.com");
     });
 
     test("With new `password`", async () => {
