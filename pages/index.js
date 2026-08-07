10f0744dd619f207fa33be8c0daccabb689812c0
Ramon Souza <ramon291997@gmail.com>
2026-08-07 15:51:13 -0300

    feat: use `DefaultLayout` on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import DefaultLayout from "interface/DefaultLayout";
+
 function Home() {
-  return <h1>meme de 🐦</h1>;
+  return (
+    <DefaultLayout
+      metadata={{
+        description: "Site em construção",
+      }}
+    >
+      <h1>meme de 🐦</h1>
+    </DefaultLayout>
+  );
 }
 
 export default Home;
